8315d67a76900e68b8e39cc596cbc3eefa02859c
Scarlett Tejada <scarlettsteph07@gmail.com>
2018-08-12 07:04:57 -0600

    update organizers and supporters sections to contain images only

--- src/components/HomePageSections/OrganizersSection.js
+++ src/components/HomePageSections/OrganizersSection.js
@@ -1,47 +1,67 @@
 import React from 'react'
-import styled from 'styled-components'
 
-import { HomePageSection } from './'
+import StyledSlatOuter from '../StyledSlatOuter'
+import StyledSlatInner from '../StyledSlatInner'
 import { EXTERNAL_LINKS } from '../../lib/appConstants'
 
 export const OrganizersSection = () => {
   return (
-    <HomePageSection
-      title='Organizan'
-      slug='organizan'
-      headerImage='url(images/logo--es-us-embassy.png)'
-      url={EXTERNAL_LINKS.ES_US_EMBASSY}
-    >
-      <OrganizersSectionStyles className='organizers'>
-        <a href={EXTERNAL_LINKS.KORINVER} target='_blank' rel='noopener noreferrer'>
-          <div className='organizers__logo organizers__logo--korinver'></div>
-        </a>
-        <a href={EXTERNAL_LINKS.CCSA} target='_blank' rel='noopener noreferrer'>
-          <div className='organizers__logo organizers__logo--ccsa'></div>
-        </a>
-        <a href={EXTERNAL_LINKS.AMERICAN_SPACES} target='_blank' rel='noopener noreferrer'>
-          <div className='organizers__logo organizers__logo--amspaces'></div>
-        </a>
-      </OrganizersSectionStyles>
-    </HomePageSection>
+    <OrganizersSectionStyles className='organizers' id='organizan'>
+      <StyledSlatInner className='organizers__inner'>
+        <h2 className='organizers__title'>Organizan</h2>
+        <div className='organizers__logos'>
+          <a href={EXTERNAL_LINKS.ES_US_EMBASSY} target='_blank' rel='noopener noreferrer'>
+            <div className='organizers__logo organizers__logo--es-us-embassy'></div>
+          </a>
+          <a href={EXTERNAL_LINKS.KORINVER} target='_blank' rel='noopener noreferrer'>
+            <div className='organizers__logo organizers__logo--korinver'></div>
+          </a>
+          <a href={EXTERNAL_LINKS.CCSA} target='_blank' rel='noopener noreferrer'>
+            <div className='organizers__logo organizers__logo--ccsa'></div>
+          </a>
+          <a href={EXTERNAL_LINKS.AMERICAN_SPACES} target='_blank' rel='noopener noreferrer'>
+            <div className='organizers__logo organizers__logo--amspaces'></div>
+          </a>
+        </div>
+      </StyledSlatInner>
+    </OrganizersSectionStyles>
   )
 }
 
-const OrganizersSectionStyles = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 50px;
-
+const OrganizersSectionStyles = StyledSlatOuter.extend`
+  border-bottom: solid 10px #fafafa;
   .organizers {
+    &__title {
+      font-size: 35px;
+      text-align: left;
+      margin: 0px;
+      margin-bottom: 20px;
+    }
+
+    &__logos {
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+    }
+
     &__header {
       margin-bottom: 30px;
     }
+
     &__logo {
-      width: 230px;
-      height: 230px;
+      width: 120px;
+      height: 120px;
       background-size: contain;
       background-position: center;
       background-repeat: no-repeat;
+      @media (min-width: ${({ theme }) => theme.large.start}) {
+        width: 250px;
+        height: 250px;
+      }
+
+      &--es-us-embassy {
+        background-image: url('/images/logo--es-us-embassy.png');
+      }
 
       &--korinver {
         background-image: url('/images/logo--korinver.png');
--- src/components/HomePageSections/SupportersSection.js
+++ src/components/HomePageSections/SupportersSection.js
@@ -1,48 +1,64 @@
 import React from 'react'
-import styled from 'styled-components'
 
-import { HomePageSection } from './'
+import StyledSlatOuter from '../StyledSlatOuter'
+import StyledSlatInner from '../StyledSlatInner'
 import { EXTERNAL_LINKS } from '../../lib/appConstants'
 
 export const SupportersSection = () => {
   return (
-    <HomePageSection
-      title='Con el apoyo de'
-      slug='colaboradores'
-      headerImage='url(images/logo--kadevjo.png)'
-      url={EXTERNAL_LINKS.KADEVJO}
-    >
-      <SupportersSectionStyles className='supporters'>
-        <a href={EXTERNAL_LINKS.VERTEX} target='_blank' rel='noopener noreferrer'>
-          <div className='supporters__logo supporters__logo--vertex'/>
-        </a>
-        <a href={EXTERNAL_LINKS.CASS} target='_blank' rel='noopener noreferrer'>
-          <div className='supporters__logo supporters__logo--cass'/>
-        </a>
-        <a href={EXTERNAL_LINKS.ART_CODE} target='_blank' rel='noopener noreferrer'>
-          <div className='supporters__logo supporters__logo--art-code'/>
-        </a>
-        <a href={EXTERNAL_LINKS.ABEU} target='_blank' rel='noopener noreferrer'>
-          <div className='supporters__logo supporters__logo--abeu'/>
-        </a>
-      </SupportersSectionStyles>
-    </HomePageSection>
+    <SupportersSectionStyles className='supporters'>
+      <StyledSlatInner className='supporters__inner'>
+        <h2 className='supporters__title'>Con el Apoyo de</h2>
+        <div className='supporters__logos'>
+          <a href={EXTERNAL_LINKS.ABEU} target='_blank' rel='noopener noreferrer'>
+            <div className='supporters__logo supporters__logo--abeu'/>
+          </a>
+          <a href={EXTERNAL_LINKS.KADEVJO} target='_blank' rel='noopener noreferrer'>
+            <div className='supporters__logo supporters__logo--kadevjo'/>
+          </a>
+          <a href={EXTERNAL_LINKS.VERTEX} target='_blank' rel='noopener noreferrer'>
+            <div className='supporters__logo supporters__logo--vertex'/>
+          </a>
+          <a href={EXTERNAL_LINKS.CASS} target='_blank' rel='noopener noreferrer'>
+            <div className='supporters__logo supporters__logo--cass'/>
+          </a>
+          <a href={EXTERNAL_LINKS.ART_CODE} target='_blank' rel='noopener noreferrer'>
+            <div className='supporters__logo supporters__logo--art-code'/>
+          </a>
+        </div>
+      </StyledSlatInner>
+    </SupportersSectionStyles>
   )
 }
 
-const SupportersSectionStyles = styled.div`
+const SupportersSectionStyles = StyledSlatOuter.extend`
   display: flex;
   justify-content: space-between;
-  margin-top: 30px;
-  margin-top: 125px;
 
   .supporters {
+    &__title {
+      font-size: 35px;
+      text-align: left;
+      margin: 0px;
+      margin-bottom: 20px;
+    }
+
+    &__logos {
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+    }
+
     &__logo {
-      width: 150px;
-      height: 150px;
+      width: 90px;
+      height: 90px;
       background-size: contain;
       background-position: center;
       background-repeat: no-repeat;
+      @media (min-width: ${({ theme }) => theme.large.start}) {
+        width: 150px;
+        height: 150px;
+      }
 
       &--kadevjo {
         background-image: url('/images/logo--kadevjo.png');
