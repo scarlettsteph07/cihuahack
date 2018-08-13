3e4610fac51a12d286041a7b06fbbfa015f852ac
Scarlett Tejada <scarlettsteph07@gmail.com>
2018-08-13 09:58:28 -0600

    update prizes section

--- src/components/HomePageSections/PrizesSection.js
+++ src/components/HomePageSections/PrizesSection.js
@@ -18,6 +18,7 @@ export const PrizesSection = () => {
     <HomePageSection title='Premios' slug='premios'>
       <PrizesSectionsStyles>
         <div className='prizes__text'>
+          Los premios incluyen:
         </div>
         <div className='prizes__carousel'>
           <Slider {...settings} className='prizes__carousel'>
@@ -27,7 +28,7 @@ export const PrizesSection = () => {
             </div>
             <div>
               <div className='prizes__carousel__image prizes__carousel__image--laptop'></div>
-              <h3 className='prizes__carousel__text'>Laptops</h3>
+              <h3 className='prizes__carousel__text'>oculus ready laptops</h3>
             </div>
           </Slider>
         </div>
@@ -40,6 +41,7 @@ const PrizesSectionsStyles = styled.div`
   .prizes {
     &__text {
       margin-bottom: 30px;
+      font-weight: 600;
     }
     &__carousel{
       &__text {
@@ -56,6 +58,7 @@ const PrizesSectionsStyles = styled.div`
         width: 300px;
         height: 200px;
         background-size: 300px;
+        background-position: center;
         }
         &--oculus {
           background-image: url(/images/oculus.png);
