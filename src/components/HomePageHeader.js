282ae94e4bbbe5531e0bfe2b4876d128e355df6d
Joel Cortez <joelalexander10@outlook.com>
2018-09-05 07:20:16 -0600

    adding aside section

--- src/components/HomePageHeader.js
+++ src/components/HomePageHeader.js
@@ -19,6 +19,13 @@ export class HomePageHeader extends Component {
     return (
       <HomePageHeaderStyles className='home-page-header__outer' id='cihuahack'>
         <StyledSlatInner className='home-page-header__inner'>
+          <HomePageHeaderAside>
+            <h1>Cihuahack</h1>
+            <h2>Gira Universitaria y lanzamiento</h2>
+            <h4>
+              Cihuahack se fue de gira, retando a jóvenes universitarios a unirse a la hackaton por un cihuatán virtual.
+            </h4>
+          </HomePageHeaderAside>
           <Slider {...settings} className='home-page-header__carousel carousel'>
             {
               images.map((img, i) => 
@@ -65,6 +72,7 @@ const HomePageHeaderStyles = StyledSlatOuter.extend`
         bottom: 25px;
       }
       .slick-dots li {
+        z-index: 9999 !important;
         margin: 0 10px;
         button {
         &::before {
@@ -90,5 +98,61 @@ const BackgroundImage = styled.div`
   }
   @media (min-width: ${({ theme }) => theme.large.start}) {
     height: 645px;
+  }
+`
+const HomePageHeaderAside = styled.div`
+  background-color: rgba(0,0,0,0.5);
+  box-shadow: 2px 2px 3px rgba(0,0,0,0.3);
+  color: ${({ theme}) => theme.white};
+  position: absolute;
+  margin-top: 65px;
+  height: 235px;
+  width: 200px;
+  z-index: 999;
+  h1 {
+      margin: 10px 0 0px 0;
+      font-size: 2em;
+    }
+  h2 {
+    font-size: 1.1em;
+  }
+  h4 {
+    margin: 0 auto;
+    width: 80%;
+    font-weight: 400;
+    font-size: 0.95em;
+  }
+  @media (min-width: ${({ theme }) => theme.medium.start}) {
+    width: 300px;
+    margin-top: 65px;
+    height: 385px;
+
+    h1 {
+      margin: 40px 0 0px 0;
+      font-size: 3em;
+    }
+    h2 {
+      font-size: 1.5em;
+    }
+    h4 {
+      width: 75%;
+      font-size: 1.35em;
+    }
+  }
+  @media (min-width: ${({ theme }) => theme.large.start}) {
+    width: 400px;
+    margin-top: 90px;
+    height: 555px;
+    h1 {
+      margin: 85px 0 0px 0;
+      font-size: 4.5em;
+    }
+    h2 {
+      font-size: 2em;
+    }
+    h4 {
+      width: 50%;
+      font-size: 1.5em;
+    }
   }
 `
